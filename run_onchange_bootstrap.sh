82370b3fd169347d7488f8311fcfd1c945dca602
qfjz <red@qfj.pl>
2024-06-30 16:59:00 +0200

    no comment

---
+++ run_onchange_bootstrap.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+if command -v ansible-playbook &> /dev/null; then
+    ansible-playbook {{ joinPath .chezmoi.sourceDir "dot_bootstrap/setup.yml" | quote }} --ask-become-pass
+fi
