a6f3135632c5d2bdc55bd155ef23a53ed6253cb4
qfjz <red@qfj.pl>
2024-06-30 17:07:16 +0200

    no comment

--- run_onchange_bootstrap.sh
+++ run_onchange_bootstrap.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 if command -v ansible-playbook &> /dev/null; then
-    ansible-playbook {{ joinPath .chezmoi.sourceDir "dot_bootstrap/setup.yml" | quote }} --ask-become-pass
+    # ansible-playbook {{ joinPath .chezmoi.sourceDir "dot_bootstrap/setup.yml" | quote }} --ask-become-pass
+    ansible-playbook "$HOME/.local/share/chezmoi/dot_bootstrap/setup.yml" --ask-become-pass
 fi
