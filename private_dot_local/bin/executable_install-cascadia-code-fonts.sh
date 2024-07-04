5decb8082dde1cad6e201f808bf0a3080778351e
qfjz <red@qfj.pl>
2024-07-04 21:17:18 +0200

    no comment

---
+++ private_dot_local/bin/executable_install-cascadia-code-fonts.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+mv ~/.fonts/ttf/*.ttf ~/.fonts/
+rm -rf ~/.fonts/static
+rm ~/.fonts/OFL.txt
+rm ~/.fonts/README.md
+rm -rf ~/.fonts/ttf
+rm -rf ~/.fonts/woff2
+rm -rf ~/.fonts/otf
