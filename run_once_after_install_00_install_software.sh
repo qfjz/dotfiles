513ce9ab5ce8dd3f60a8d0e73174b105370ca733
qfjz <red@qfj.pl>
2024-07-14 14:37:58 +0200

    no comment

--- run_once_after_install_00_install_software.sh
+++ run_once_after_install_00_install_software.sh
@@ -4,45 +4,65 @@
 
 shopt -s nocasematch
 
-read -p "Czy chcesz zainstalować pakiety i3 WM [t/n]: " -n1 ODP
+read -p "Czy chcesz zainstalować pakiety i3 WM [T/n]: " -n1 ODP
 case $ODP in
     t)
+        echo
+        $HOME/bin/install-i3.sh
+        ;;
+    *)
         echo
         $HOME/bin/install-i3.sh
         ;;
 esac
 
 echo
-read -p "Czy chcesz zainstalować pakiety KDE [t/n]: " -n1 ODP
+read -p "Czy chcesz zainstalować pakiety KDE [T/n]: " -n1 ODP
 case $ODP in
     t)
+        echo
+        $HOME/bin/install-kde.sh
+        ;;
+    *)
         echo
         $HOME/bin/install-kde.sh
         ;;
 esac
 
 echo
-read -p "Czy chcesz zainstalować pakiety devel [t/n]: " -n1 ODP
+read -p "Czy chcesz zainstalować pakiety devel [T/n]: " -n1 ODP
 case $ODP in
     t)
+        echo
+        $HOME/bin/install-kde.sh
+        ;;
+    *)
         echo
         $HOME/bin/install-kde.sh
         ;;
 esac
 
 echo
-read -p "Czy chcesz zainstalować flatpak i dodatkowe oprogramowanie [t/n]: " -n1 ODP
+read -p "Czy chcesz zainstalować flatpak i dodatkowe oprogramowanie [T/n]: " -n1 ODP
 case $ODP in
     t)
+        echo
+        $HOME/bin/install-kde.sh
+        ;;
+    *)
         echo
         $HOME/bin/install-kde.sh
         ;;
 esac
 
 echo
-read -p "Czy chcesz zainstalować czcionki [t/n]: " -n1 ODP
+read -p "Czy chcesz zainstalować czcionki [T/n]: " -n1 ODP
 case $ODP in
     t)
+        echo
+        $HOME/bin/install-kde.sh
+        ;;
+    *)
         echo
         $HOME/bin/install-kde.sh
         ;;
